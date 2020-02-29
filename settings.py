fa866ad54de3caaca77d7aab0979a9359cc62225
Roma Piskunov <roman_piskunov@bk.ru>
2020-03-01 04:21:30 +0500

    change settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 URL_PARSE = ['https://freedom-stat.com/']
-EMAIL = ['tidokep225@mailimail.com']
-PASSWORD = ['fxmue}hF']
+EMAIL = ['awdawdawd@x3mailer.com']
+PASSWORD = ['2moZlxu*']
 
 SENDGRID_API_KEY = ['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']
 FROM_EMAIL = ['mr_volland@mail.ru']
